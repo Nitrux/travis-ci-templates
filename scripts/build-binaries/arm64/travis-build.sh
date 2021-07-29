1eac0fced4e1af04ef31ffdb7eda15e40524857f
Uri Herrera <hello@uriherrera.com>
2021-07-29 12:42:15 -0500

    add files

---
+++ scripts/build-binaries/arm64/travis-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+apt -qq update
+apt -qq -yy install equivs curl git wget gnupg2
+
+### Install Dependencies
+
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream
+
+mk-build-deps -i -t "apt-get --yes" -r
+
+### Clone repo.
+
+### ADD CLONE COMMANDS HERE OR ADD SOURCE TO REPO
+
+### Build Deb
+
+mkdir source
+mv ./* source/ # Hack for debuild
+cd source
+debuild -b -uc -us
